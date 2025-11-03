9a066defc796ee4774625563a23dc35d6c691c4b
Coder-Aman-868 <developeraman868@gmail.com>
2025-11-03 15:22:37 +0530

    header-static completed

--- src/components/common/Cta.tsx
+++ src/components/common/Cta.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Cta = () => {
-  return (
-    <div>Cta</div>
-  )
+interface CtaProps {
+    children: React.ReactNode;
+    className?: string;
+    onClick?: () => void;
+}
+
+const Cta = ({ children, className, onClick }: CtaProps) => {
+    return (
+        <button onClick={onClick} className={`py-3 px-4 bg-off-gold text-off-black rounded-xs text-custom-xs leading-130 satoshi tracking-[1.5px] uppercase hover:text-off-gold hover:bg-transparent border-off-gold border duration-300 ${className}`}>{children}</button>
+    )
 }
 
 export default Cta
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Cta from './Cta'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div className='fixed top-12 left-1/2 -translate-x-1/2 p-3 pl-6 border border-off-gold/15 rounded-xs bg-off-gold/5 backdrop-blur-[30px] '>
+            <div className='flex items-center gap-8'>
+                <h4 className='text-xl leading-100 satoshi'>Aman.</h4>
+                <div className='satoshi flex items-center gap-4'>
+                    <ul className='flex items-center gap-5 text-custom-xs leading-130 uppercase tracking-[1.5px]'>
+                        <li>Services</li>
+                        <li>Work</li>
+                        <li>About</li>
+                        <li>Pages</li>
+                    </ul>
+                    <Cta>Let’s talk</Cta>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
