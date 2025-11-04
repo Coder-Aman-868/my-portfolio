6e12610628336b30c2302baa7d42b81092b178b7
Coder-Aman-868 <developeraman868@gmail.com>
2025-11-04 12:14:23 +0530

    TimeLine static completed

--- src/components/home/TimeLine.tsx
+++ src/components/home/TimeLine.tsx
@@ -1,8 +1,46 @@
+import { timeLineData } from '@/utils/Helper'
 import React from 'react'
+import Paragraph from '../common/Paragraph'
+import Heading from '../common/Heading'
 
 const TimeLine = () => {
+  const timeLineIndex = []
+  for (let i = 1; i < timeLineData.length + 1; i++) {
+    timeLineIndex.push(i)
+  }
   return (
-    <div>TimeLine</div>
+    <div className='px-5 pt-28 pb-40'>
+      <div className="max-w-[1400px] mx-auto">
+        <div className="">
+          {
+            timeLineData.map((item, index) => (
+              <div key={index} className={`w-full flex relative ${index % 2 !== 0 ? "justify-start" : "justify-end"} ${index === 0 ? "mt-0" : "-mt-12"}`}>
+                <div className="w-[45%] border border-off-gold/15 bg-off-gold/5 rounded-xs backdrop-blur-[10px] p-12">
+                  <Paragraph className='uppercase text-custom-xs! tracking-[1.5px] opacity-60 text-start leading-160!'>{item.title}</Paragraph>
+                  <Heading className='text-2xl! font-light tracking-[1px] uppercase mt-1'>{item.heading}</Heading>
+                  <Paragraph className='mt-4 text-base! text-start leading-180 mb-8'>{item.paragraph}</Paragraph>
+                  <ul className='list-disc pl-5 space-y-4'>
+                    {
+                      item.list.map((listItem, listIndex) => (
+                        <li className='chillax leading-180' key={listIndex}>{listItem}</li>
+                      ))
+                    }
+                  </ul>
+                </div>
+                <div className="absolute top-0 left-1/2 -translate-x-1/2 h-full flex items-center gap-4 flex-col justify-center">
+                  <div className="border border-off-gold/15 h-full"></div>
+                  <div className="min-h-10 min-w-10 flex justify-center items-center rounded-[999px] border border-off-gold/15 bg-off-gold/5">
+                    <span className='satoshi text-custom-xs! tracking-[1.5px]  leading-160'>0{index + 1}</span>
+                  </div>
+                  <div className={`border border-off-gold/15 h-full ${index === timeLineData.length - 1 ? "opacity-0 pointer-events-none" : "flex"}`}></div>
+                </div>
+              </div>
+            ))
+          }
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -14,26 +14,77 @@ export const QualificationData = [
         title: "Content & Seo",
         paragraph: "Proven SEO strategies that enhance your online performance, bringing you to the forefront of organic search results.",
     },
-]
-export const SelectedWorkData =[
+];
+export const SelectedWorkData = [
     {
-        title : "Space",
-        paragraph:"Webdesign",
-        imageUrl:"/assets/images/png/selected-work-image-1.png",
+        title: "Space",
+        paragraph: "Webdesign",
+        imageUrl: "/assets/images/png/selected-work-image-1.png",
     },
     {
-        title : "Nova",
-        paragraph:"Webdesign",
-        imageUrl:"/assets/images/png/selected-work-image-2.png",
+        title: "Nova",
+        paragraph: "Webdesign",
+        imageUrl: "/assets/images/png/selected-work-image-2.png",
     },
     {
-        title : "Sonic",
-        paragraph:"Webdesign",
-        imageUrl:"/assets/images/png/selected-work-image-3.png",
+        title: "Sonic",
+        paragraph: "Webdesign",
+        imageUrl: "/assets/images/png/selected-work-image-3.png",
     },
     {
-        title : "Solar",
-        paragraph:"Webdesign",
-        imageUrl:"/assets/images/png/selected-work-image-4.png",
+        title: "Solar",
+        paragraph: "Webdesign",
+        imageUrl: "/assets/images/png/selected-work-image-4.png",
+    },
+];
+export const timeLineData = [
+    {
+        title: "Do we Match?",
+        heading: "Discovery Call",
+        paragraph: "Before we start, we determine if and how I can help you. What are your requirements for your new website? Why do you need a new website? What goals do you have, and what problems can we solve with a new website?",
+        list: [
+            "We get to know each other better",
+            "Determine how I can best assist you",
+            "Understand the goals you have for your website"
+        ]
+    },
+    {
+        title: "lorem ipsum",
+        heading: "Concept & STrategy",
+        paragraph: "Together, we develop a strategy that successfully combines your goals with the needs of your target audience. Based on this concept, I create the first wireframes and an interactive prototype. This provides us with a very good impression of the website and the user interface.",
+        list: [
+            "UX Design",
+            "Wireframes",
+            "Interactive Prototype"
+        ]
+    },
+    {
+        title: "SOme MAgic",
+        heading: "web Design",
+        paragraph: "Now comes the magic. Based on the previously developed concept, I create a high-end screen design perfectly tailored to your brand. A web design that sets you apart from your competition, fits your target audience ideally, and provides an excellent user experience.",
+        list: [
+            "High-end web design tailored to your brand",
+            "Interactive prototype of the design"
+        ]
+    },
+    {
+        title: "More Magic",
+        heading: "Development",
+        paragraph: "In this step, we breathe life into your new high-end design. You will receive a custom-built website using a modular web design system and CMS integration. Animations will add the necessary flair to your site and set you apart from the boring competition.",
+        list: [
+            "Custom framer website",
+            "Modular web design systems",
+            "CMS integration"
+        ]
+    },
+    {
+        title: "ready to go",
+        heading: "WEbsite onboarding",
+        paragraph: "In a personal Framer workshop, I will show you how to make changes to your new website quickly and easily. Additionally, you will receive personalized Framer video tutorials that you can access at any time. Edit your Framer website without a complicated backend or the need for an additional programmer. It's as simple as that.",
+        list: [
+            "Personal workshop",
+            "Personalized video tutorials",
+            "Edit text and images directly on your website"
+        ]
     },
 ]
