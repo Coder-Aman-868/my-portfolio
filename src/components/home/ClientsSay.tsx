dfd55d12d75fb056cebb20d6a4e70ff6103d74f9
Coder-Aman-868 <developeraman868@gmail.com>
2025-11-08 09:57:32 +0530

    client says animation completed

--- src/components/home/ClientsSay.tsx
+++ src/components/home/ClientsSay.tsx
@@ -1,22 +1,95 @@
 "use client";
-import React from 'react'
+import React, { use, useEffect, useRef } from 'react'
 import Heading from '../common/Heading'
 import Paragraph from '../common/Paragraph'
 import Image from 'next/image'
 import { clientsData } from '@/utils/Helper'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger)
 
 const ClientsSay = () => {
+  const mainHeadiingRef = useRef(null)
+  const mainHeadiingSpanRef = useRef(null)
+  const paragraphRef = useRef(null)
+  const cantainerRef = useRef(null)
+  const boxRef = useRef<(HTMLDivElement | null)[]>([])
+
+  useEffect(() => {
+    gsap.fromTo(mainHeadiingRef.current,
+      {
+        opacity: 0,
+        x: -100,
+        filter: "blur(10px)",
+      },
+      {
+        opacity: 1,
+        x: 0,
+        filter: "blur(0px)",
+        duration: 3,
+        scrollTrigger: {
+          trigger: mainHeadiingRef.current,
+          start: "top 80%",
+          end: "top 60%",
+          scrub: 1,
+          toggleActions: "play none none reverse"
+        },
+        ease: "back.out(1.7)"
+      });
+    gsap.fromTo(mainHeadiingSpanRef.current,
+      {
+        opacity: 0,
+        x: 100,
+        filter: "blur(10px)",
+      },
+      {
+        opacity: 1,
+        x: 0,
+        filter: "blur(0px)",
+        duration: 3,
+        scrollTrigger: {
+          trigger: mainHeadiingSpanRef.current,
+          start: "top 80%",
+          end: "top 60%",
+          scrub: 1,
+          toggleActions: "play none none reverse"
+        },
+        ease: "back.out(1.7)"
+      });
+    gsap.fromTo(paragraphRef.current,
+      {
+        opacity: 0,
+        y: 100,
+        filter: "blur(10px)",
+      },
+      {
+        opacity: 1,
+        y: 0,
+        filter: "blur(0px)",
+        duration: 3,
+        scrollTrigger: {
+          trigger: paragraphRef.current,
+          start: "top 80%",
+          end: "top 60%",
+          scrub: 1,
+          toggleActions: "play none none reverse"
+        },
+        ease: "back.out(1.7)"
+      });
+  }, [])
   return (
     <div className='px-5'>
       <div className="mx-auto max-w-[1200px]">
         <div className="max-w-[600px] mx-auto md:mb-16 mb-10">
-          <Heading className='lg:text-9xl! md:text-8xl! sm:text-6xl! text-5xl! satoshi font-light leading-100! text-center sm:mb-4 mb-3'>What my <br /> <span className='italic'>clients say</span></Heading>
-          <Paragraph className='sm:text-lg! text-base!'>See what my clients have to say about working with me and the results I helped them achieve.</Paragraph>
+          <Heading ref={mainHeadiingRef} className='lg:text-9xl! md:text-8xl! sm:text-6xl! text-5xl! satoshi font-light leading-100! text-center sm:mt-4 mt-3'>What my</Heading>
+          <Heading ref={mainHeadiingSpanRef} className='lg:text-9xl! md:text-8xl! sm:text-6xl! text-5xl! satoshi font-light leading-100! text-center sm:mb-4 mb-3 italic'>clients say</Heading>
+          <Paragraph ref={paragraphRef} className='sm:text-lg! text-base!'>See what my clients have to say about working with me and the results I helped them achieve.</Paragraph>
         </div>
-        <div className="w-full grid lg:grid-cols-2 sm:gap-8 gap-5">
+        <div ref={cantainerRef} className="w-full grid lg:grid-cols-2 sm:gap-8 gap-5">
           {
             clientsData.map((item, index) => (
-              <div key={index} className="border border-off-gold/15 bg-off-gold/5 backdrop-blur-[10px] md:p-12 sm:p-9 p-6 rounded-xs">
+              <div ref={(el) => { boxRef.current[index] = el }} key={index} className="border border-off-gold/15 bg-off-gold/5 backdrop-blur-[10px] md:p-12 sm:p-9 p-6 rounded-xs">
                 <Image
                   src={item.image}
                   alt={item.heading}
--- src/components/home/TimeLine.tsx
+++ src/components/home/TimeLine.tsx
@@ -10,90 +10,51 @@ gsap.registerPlugin(ScrollTrigger)
 
 const TimeLine = () => {
   const boxRef = useRef<(HTMLDivElement | null)[]>([])
+  const numberRef = useRef<(HTMLDivElement | null)[]>([])
 
   useEffect(() => {
-    boxRef.current.forEach((box, index) => {
-      if (index % 2 !== 0) {
-        gsap.fromTo(box,
-          {
-            opacity: 0,
-            x: -100,
-            scale: 0.95,
-            y: 50,
-            filter: "blur(10px)",
-          },
-          {
-            opacity: 1,
-            x: 0,
-            scale: 1,
-            y: 0,
-            filter: "blur(0px)",
-            duration: 3,
-            scrollTrigger: {
-              trigger: box,
-              start: "top 80%",
-              end: "top 60%",
-              scrub: 1,
-              toggleActions: "play none none reverse"
-            },
-            ease: "back.out(1.7)"
-          }
-        )
-      } else {
-        gsap.fromTo(box,
-          {
-            opacity: 0,
-            x: 100,
-            scale: 0.95,
-            y: 50,
-            filter: "blur(10px)",
-          },
-          {
-            opacity: 1,
-            x: 0,
-            scale: 1,
-            y: 0,
-            filter: "blur(0px)",
-            duration: 3,
-            scrollTrigger: {
-              trigger: box,
-              start: "top 80%",
-              end: "top 60%",
-              scrub: 1,
-              toggleActions: "play none none reverse"
-            },
-            ease: "back.out(1.7)"
-          }
-        )
-      }
-    })
+    boxRef.current.forEach(box => {
+      gsap.to(box, {
+        scale: 0,
+        opacity: 0,
+        filter: "blur(40px)",
+        scrollTrigger: {
+          trigger: box,
+          start: "top 10%",
+          end: "bottom 10%",
+          scrub: 1,
+        },
+        duration: 3,
+        ease: "back.inOut(1.7)"
+      })
+    });
   })
 
   return (
-    <div className='px-5 lg:pt-28 md:py-20 sm:py-14 py-8 lg:pb-40 overflow-y-hidden'>
+    <div className='px-5 lg:pt-28 md:py-20 sm:py-14 py-8 lg:pb-40 overflow-y-clip'>
       <div className="max-w-[1400px] mx-auto">
-        <div className="">
+        <div className="relative">
           {
             timeLineData.map((item, index) => (
-              <div key={index} className={`w-full flex relative lg:pl-0 sm:pl-15 pl-12 ${index % 2 !== 0 ? "lg:justify-start" : "lg:justify-end"} ${index === 0 ? "mt-0" : "lg:-mt-12"}`}>
-                <div ref={(el) => { boxRef.current[index] = el }} className="lg:w-[45%] border border-off-gold/15 bg-off-gold/5 rounded-xs backdrop-blur-[10px] md:p-12 sm:p-9 p-6 mt-6">
-                  <Paragraph className='uppercase text-custom-xs! tracking-[1.5px] opacity-60 text-start leading-160!'>{item.title}</Paragraph>
-                  <Heading className='sm:text-2xl! text-xl! font-light tracking-[1px] uppercase mt-1'>{item.heading}</Heading>
-                  <Paragraph className='sm:mt-4 mt-1 sm:text-base! text-sm! text-start leading-180 sm:mb-8 mb-4'>{item.paragraph}</Paragraph>
-                  <ul className='list-disc pl-5 sm:space-y-4 space-y-1'>
-                    {
-                      item.list.map((listItem, listIndex) => (
-                        <li className='chillax leading-180 sm:text-base text-sm' key={listIndex}>{listItem}</li>
-                      ))
-                    }
-                  </ul>
-                </div>
-                <div className="absolute top-0 left-0 lg:left-1/2 lg:-translate-x-1/2 h-full flex items-center gap-4 flex-col justify-center">
-                  <div className={`border border-off-gold/15 h-full ${index === 0 ? "opacity-0 pointer-events-none" : "flex"}`}></div>
-                  <div className="sm:min-h-10 min-w-8 sm:min-w-10 min-h-8 flex justify-center items-center rounded-[999px] border border-off-gold/15 bg-off-gold/5">
-                    <span className='satoshi sm:text-custom-xs! text-xs tracking-[1.5px]  leading-160'>0{index + 1}</span>
+              <div ref={(el) => { boxRef.current[index] = el }} key={index} className=" sticky! top-14">
+                <div className={`w-full flex relative lg:pl-0 sm:pl-15 pl-12 ${index % 2 !== 0 ? "lg:justify-start" : "lg:justify-end"} ${index === 0 ? "mt-0" : "lg:-mt-12"}`}>
+                  <div className="lg:w-[45%] border border-off-gold/15 bg-off-gold/5 rounded-xs backdrop-blur-[10px] md:p-12 sm:p-9 p-6 mt-6">
+                    <Paragraph className='uppercase text-custom-xs! tracking-[1.5px] opacity-60 text-start leading-160!'>{item.title}</Paragraph>
+                    <Heading className='sm:text-2xl! text-xl! font-light tracking-[1px] uppercase mt-1'>{item.heading}</Heading>
+                    <Paragraph className='sm:mt-4 mt-1 sm:text-base! text-sm! text-start leading-180 sm:mb-8 mb-4'>{item.paragraph}</Paragraph>
+                    <ul className='list-disc pl-5 sm:space-y-4 space-y-1'>
+                      {
+                        item.list.map((listItem, listIndex) => (
+                          <li className='chillax leading-180 sm:text-base text-sm' key={listIndex}>{listItem}</li>
+                        ))
+                      }
+                    </ul>
                   </div>
-                  <div className={`border border-off-gold/15 h-full ${index === timeLineData.length - 1 ? "opacity-0 pointer-events-none" : "flex"}`}></div>
+                  <div className="absolute top-0 left-0 lg:left-1/2 lg:-translate-x-1/2 h-full flex items-center gap-4 flex-col justify-center">
+                    <div className="sm:min-h-10 min-w-8 sm:min-w-10 min-h-8 flex justify-center items-center rounded-[999px] border border-off-gold/15 bg-off-gold/5">
+                      <span className='satoshi sm:text-custom-xs! text-xs tracking-[1.5px]  leading-160'>0{index + 1}</span>
+                    </div>
+                  </div>
                 </div>
               </div>
             ))
